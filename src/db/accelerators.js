a732b8ae8c9749a3795ffd03473685fd125d16cd
Silvinecoder <creativeservicepackages@gmail.com>
2023-06-29 10:55:22 +0100

    removing credentials

--- src/db/accelerators.js
+++ src/db/accelerators.js
@@ -2,8 +2,8 @@
 // AWS.config.update({
 //   region: 'local',
 //   endpoint: 'http://dynamodb-local:8000',
-//   accessKeyId: 'ASIA5XW4FA6U25ILNFB5',
-//   secretAccessKey: 'uinVN/UQ1cKY7nar04EkexUGl6tQ8Mm3OeTCmbf2'
+//   accessKeyId:
+//   secretAccessKey:
 // });
 // const dynamoClient = new AWS.DynamoDB.DocumentClient();
 // console.log('Loading DB, please wait.');
